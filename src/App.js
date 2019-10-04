26f07017a63ee016110a1dbb9c01ae04db053d1e
Sebastián <sgarcia710@outlook.com>
2019-10-04 04:30:40 -0500

    useEffect usage with a clicker example

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Toggler from './components/Toggler';
 import SimpleFormHooks from './components/SimpleFormHooks'; // this one use useState on the component
 import SimpleFormInputHook from './components/SimpleFormInputHook'; // this one use a custom hook created in the hooks folder
+import Clicker from './components/Clicker';
 
 function App() {
   return (
     <>
-      <Toggler />
+      {/* <Toggler />
       <SimpleFormHooks />
-      <SimpleFormInputHook />
+      <SimpleFormInputHook /> */}
+      <Clicker />
     </>
   );
 }
