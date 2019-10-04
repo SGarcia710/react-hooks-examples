000bbc26c2fe7cf33a06ba1777b563543ac651cd
Sebastián <sgarcia710@outlook.com>
2019-10-04 02:17:51 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Toggler from './Toggler';
+
+function App() {
+  return (
+    <>
+      <Toggler />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Toggler.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import useToggle from './hooks/useToggle';
+
+const Toggler = () => {
+  const [isHappy, toggleIsHappy] = useToggle(true);
+  const [isHeartBroken, toggleIsHeartBroken] = useToggle(false);
+  const [isDog, toggleIsDog] = useToggle(true);
+
+  return (
+    <>
+      <h1 onClick={toggleIsHappy}> {isHappy ? '😄' : '😞'} </h1>
+      <h1 onClick={toggleIsHeartBroken}> {isHeartBroken ? '💔' : '💖'} </h1>
+      <h1 onClick={toggleIsDog}> {isDog ? '🐶' : '🐱'} </h1>
+    </>
+  );
+};
+
+export default Toggler;
